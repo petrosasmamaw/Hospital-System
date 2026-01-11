7effa8a45b45c997453dcb4b8166e289d52b447d
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-01-11 20:30:46 +0300

    update the auth

--- hospital-admin/src/components/slices/slice/authSlice.js
+++ hospital-admin/src/components/slices/slice/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // send cookies automatically
-const API_BASE =  "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const AUTHAPI_URL = `${API_BASE}/api/auth/`;
 
 export const registerUser = createAsyncThunk("auth/registerUser", async (userData) => {
